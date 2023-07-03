d48603a91540473b6e2b9de2b25de682b7afd7e1
Leiner Stanish <leiner.mena@bodytechcorp.com>
2023-07-03 10:11:47 -0500

    Creacion y estructuracion de carpetas

---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "../../components/Buttons/Button";
+
+const Home = () => { 
+    
+    return (
+        <div>
+          <h1>¡Hola, mundo!</h1>
+          <Button />
+        </div>
+      );
+
+};
+
+export default Home;
